be2895b88bcddbe962931ad33f4da88e5140b069
sena bakacak <senabakacak2@hotmail.com>
2022-03-07 18:26:46 +0300

    example

---
+++ DB_FIRST_LABS/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace DB_FIRST_LABS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(56, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1023, 196);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(56, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 62);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Example1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(203, 239);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 62);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Example2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(350, 239);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(141, 62);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Example3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(497, 239);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(141, 62);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Example4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(644, 239);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(141, 62);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Example5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button6.Location = new System.Drawing.Point(791, 239);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(141, 62);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Example6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1139, 351);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ DB_FIRST_LABS/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.SqlServer;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DB_FIRST_LABS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        NorthwindEntities db = new NorthwindEntities();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.Employees.Select(x => new
+            {
+                FirstName = x.FirstName,
+                LastName=x.LastName,
+                Age= SqlFunctions.DateDiff("Year", x.BirthDate, DateTime.Now)
+            }).ToList();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.Employees.Select(x => new
+            {
+                FirstName = x.FirstName,
+                LastName = x.LastName,
+                Age = SqlFunctions.DateDiff("Year", x.BirthDate, DateTime.Now)
+            })
+            .Where(x => x.Age > 60)
+            .OrderByDescending(x => x.Age)
+            .ToList();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+         
+            dataGridView1.DataSource = db.Products.GroupBy(x => x.Category.CategoryName)
+                                                  .Select(x => new
+                                                  {
+                                                      CategoryName = x.Key,
+                                                      Stock = x.Sum(y => y.UnitsInStock)
+                                                  })
+                                                  .OrderByDescending(x => x.Stock)
+                                                  .ToList();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.Products.GroupBy(x => x.Supplier.CompanyName)
+                                                     .Select(x => new
+                                                     {
+                                                         CompanyName = x.Key,
+                                                         Amount = x.Count()
+                                                     })
+                                                     .OrderByDescending(x => x.Amount)
+                                                     .ToList();
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.Employees.Select(x => new
+            {
+                FirstName = x.FirstName,
+                LastName = x.LastName,
+                Title = x.TitleOfCourtesy,
+                Age = SqlFunctions.DateDiff("Year", x.BirthDate, DateTime.Now)
+            })
+            .Where(x => x.Title.Contains("Mr.") || (x.Age >= 50 && x.Age <= 60))
+            .OrderByDescending(x => x.Age)
+            .ToList();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            
+            dataGridView1.DataSource = db.Products
+                .Join(db.Order_Details, p => p.ProductID, od => od.ProductID, (p, od) => new { p, od })
+                .GroupBy(x => x.p.ProductName)
+                .Select(z => new
+                {
+                    ProdcutName = z.Key,
+                    Amount = z.Sum(y => y.od.Quantity),
+                    Income = z.Sum(y => y.od.Quantity * y.od.UnitPrice)
+                })
+                .OrderByDescending(x => x.Income)
+                .ToList();
+        }
+
+       
+    }
+}
